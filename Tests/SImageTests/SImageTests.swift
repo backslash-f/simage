6a976875b6bf808700567ea94741c859e39854b0
Fernando Fernandes <fernando.fernandes@labtwin.com>
2020-02-13 00:04:50 +0100

    Add image test resources
     - Create first version of helper function that loads images from
    Resources

--- Tests/SImageTests/SImageTests.swift
+++ Tests/SImageTests/SImageTests.swift
@@ -1,12 +1,41 @@
 import XCTest
+
 @testable import SImage
 
 final class SImageTests: XCTestCase {
 
-    func testExample() {
-    }
+    // MARK: - Public Properties
 
     static var allTests = [
         ("testExample", testExample),
     ]
+}
+
+// MARK: - Tests
+
+extension SImageTests {
+
+    func testExample() {
+        let imageURL = imageSourceURLs()
+        let image = try! SImage().createImage(from: imageURL.first!)
+        print(image)
+    }
+}
+
+// MARK: - Private
+
+private extension SImageTests {
+
+    func imageSourceURLs() -> [URL] {
+        let currentFileURL = URL(fileURLWithPath: "\(#file)", isDirectory: false)
+        let resourcesPath = currentFileURL.deletingLastPathComponent().appendingPathComponent("Resources")
+        let prefix = "Portrait_"
+        var sourceURLs = [URL]()
+        for n in 0..<9 {
+            let imageURL = resourcesPath.appendingPathComponent("\(prefix)\(n).jpg", isDirectory: false)
+            print(imageURL)
+            sourceURLs.append(imageURL)
+        }
+        return sourceURLs
+    }
 }
