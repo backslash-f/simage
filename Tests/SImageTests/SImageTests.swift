9d29f0840276ce5a3a27ac9c1f7ec0f5e9729fb5
Fernando Fernandes <fernando.fernandes@labtwin.com>
2020-02-17 16:52:01 +0100

    Add testCreateCGImage()

--- Tests/SImageTests/SImageTests.swift
+++ Tests/SImageTests/SImageTests.swift
@@ -7,18 +7,34 @@ final class SImageTests: XCTestCase {
     // MARK: - Public Properties
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testExample", testCreateCGImage),
     ]
+
+    // MARK: - Private Properties
+
+    private var resourcesPath: URL {
+        let currentFileURL = URL(fileURLWithPath: "\(#file)", isDirectory: false)
+        return currentFileURL.deletingLastPathComponent().appendingPathComponent("Resources")
+    }
+
+    private var imagePrefix: String {
+        "image_"
+    }
 }
 
 // MARK: - Tests
 
 extension SImageTests {
 
-    func testExample() {
-        let imageURL = imageSourceURLs()
-        let image = try! SImage().createImage(from: imageURL.first!)
-        print(image)
+    func testCreateCGImage() {
+        let imageURL = randomImageURL()
+        do {
+            let cgImage = try SImage().createImage(from: imageURL)
+            XCTAssert(cgImage.height > 0, "Invalid height.")
+            XCTAssert(cgImage.width > 0, "Invalid width.")
+        } catch {
+            XCTFail("Cannot create an image. Error: \(error)")
+        }
     }
 }
 
@@ -26,13 +42,16 @@ extension SImageTests {
 
 private extension SImageTests {
 
+    func randomImageURL() -> URL {
+        let randomImageNumber = (0..<9).randomElement() ?? 0
+        let imageName = "\(imagePrefix)\(randomImageNumber).jpg"
+        return resourcesPath.appendingPathComponent(imageName, isDirectory: false)
+    }
+
     func imageSourceURLs() -> [URL] {
-        let currentFileURL = URL(fileURLWithPath: "\(#file)", isDirectory: false)
-        let resourcesPath = currentFileURL.deletingLastPathComponent().appendingPathComponent("Resources")
-        let prefix = "Portrait_"
         var sourceURLs = [URL]()
         for n in 0..<9 {
-            let imageURL = resourcesPath.appendingPathComponent("\(prefix)\(n).jpg", isDirectory: false)
+            let imageURL = resourcesPath.appendingPathComponent("\(imagePrefix)\(n).jpg", isDirectory: false)
             print(imageURL)
             sourceURLs.append(imageURL)
         }
